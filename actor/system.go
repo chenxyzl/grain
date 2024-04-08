2d57af427237405d4297eee8319a3a3bff01731c
lin <ichenzhl@qq.com>
2024-04-08 19:22:51 +0800

    temp

--- actor/system.go
+++ actor/system.go
@@ -2,18 +2,28 @@ package actor
 
 type System struct {
 	config   *Config
-	kinds    map[string]tKind
 	registry *Registry
 }
 
 func NewSystem(config *Config) *System {
 	system := &System{}
 	system.config = config
-	system.kinds = make(map[string]tKind)
 	system.registry = newRegistry(system)
 	return system
 }
 
 func (x *System) Start() error {
 	return nil
+}
+
+func (x *System) Stop() error {
+	return nil
+}
+
+func (x *System) GetConfig() *Config {
+	return x.config
+}
+
+func (x *System) GetRegistry() *Registry {
+	return x.registry
 }
