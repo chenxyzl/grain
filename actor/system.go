86faf6a9ad46d0760be75b12aaf68b4eee0283af
lin <ichenzhl@qq.com>
2024-05-24 11:16:00 +0800

    [op] remove unused code

--- actor/system.go
+++ actor/system.go
@@ -69,12 +69,7 @@ func (x *System) SpawnNamed(p Producer, name string, opts ...KindOptFunc) *Actor
 }
 
 func (x *System) GetRemoteActorRef(kind string, name string) *ActorRef {
-	addr := ""
-	if kind == defaultLocalKind {
-		addr = x.clusterProvider.addr()
-	} else {
-		addr = x.calcAddressByKind7Id(x.clusterProvider.getNodes(), kind, name)
-	}
+	addr := x.calcAddressByKind7Id(x.clusterProvider.getNodes(), kind, name)
 	//
 	if addr == "" {
 		return nil
